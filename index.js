ba5f1dd1104de7b72d5f7a7579381cda0f0afa0c
Omid Hosseini Dehkordi <omid@Omids-MacBook-Air.local>
2022-06-22 10:28:56 -0700

    Link Sidebar to HTML Files and Start Working on Google Sign Up API

---
+++ index.js
@@ -0,0 +1,39 @@
+function changeWindow(tab) 
+{
+    if(tab == 'home')
+    {
+        window.location.href = "index.html";
+    }
+    if(tab == 'locations')
+    {
+        window.location.href = "locations.html";
+    }
+    if(tab == 'watch')
+    {
+        window.location.href = "watch.html";
+    }
+    if(tab == 'favoritesports')
+    {
+        window.location.href = "favoritesports.html";
+    }
+    if(tab == 'messages')
+    {
+        window.location.href = "messages.html";
+    }
+    if(tab == 'notifications')
+    {
+        window.location.href = "notifications.html";
+    }
+    if(tab == 'upcoming')
+    {
+        window.location.href = "upcomingevents.html";
+    }
+    if(tab == 'settings')
+    {
+        window.location.href = "settings.html";
+    }
+    if(tab == 'signin')
+    {
+        window.location.href = "signin.html";
+    }
+}
