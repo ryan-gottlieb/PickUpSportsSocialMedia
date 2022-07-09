43f9b1b48f9b915121cabb1d6e4bc443863499f8
Omid Hosseini Dehkordi <omid@Omids-MacBook-Air.local>
2022-07-09 12:51:02 -0700

    Django initialized (Still need to work on loading all HTML files)

---
+++ pickupsports/pickup/templates/index.js
@@ -0,0 +1,29 @@
+function changeWindow(tab) 
+{
+    if(tab == 'home')
+    {
+        window.location.href = "index.html";
+    }
+    if(tab == 'locations')
+    {
+        window.location.href = "locations.html";
+    }
+    if(tab == 'watch')
+    {
+        window.location.href = "watch.html";
+    }
+    if(tab == 'favoritesports')
+    {
+        window.location.href = "favoritesports.html";
+    }
+    if(tab == 'upcoming')
+    {
+        window.location.href = "upcomingevents.html";
+    }
+    if(tab == 'settings')
+    {
+        window.location.href = "settings.html";
+    }
+}
+
+
